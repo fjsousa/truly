537c4e273a3e71e0620224f6040a3825d065427f
Flávio Sousa <flavio.jsousa@gmail.com>
2014-02-09 14:40:59 +0000

    duplicated email

--- web.js
+++ web.js
@@ -29,33 +29,62 @@ var connection = mysql.createConnection({
 connection.connect();
 
 app.post('/create', function (req, res) {
-  if (!req.body)
+  if (!req.body) {
     return res.send(409, 'No content in body');
+  }
+  console.log('Create');
 
   var email = req.body.email;
   var name = req.body.name;
+  var type = req.body.type;
+  var gender = req.body.gender;
+  var size = req.body.size.toString();
+  var org = req.body.org;
+  var local = req.body.local;
+  var img = req.body.img;
 
-  var query = 'insert into person (name, email) values(' +
-    '"' + name  + '",' +
-    '"' + email + '");';
+  var query = 'select count(*) from person where email = "' + email + '"';
 
-    connection.query(query, function(err, rows, fields) {
-      if (err) throw err;
+  connection.query(query, function(err, rows, fields) {
+    if (err) { res.send(500, err);}
 
-      var type = req.body.type;
-      var gender = req.body.gender;
-      var size = req.body.size.toString();
-      var org = req.body.org;
-      var local = req.body.local;
-      var img = req.body.img;
+    var mailCount = rows[0]['count(*)'];
+    console.log('Received request. Mail # in db:', mailCount);
+
+    if (mailCount === 0 ) {
+
+      query = 'insert into person (name, email) values(' +
+        '"' + name  + '",' +
+        '"' + email + '");';
+
+      connection.query(query, function(err, rows, fields) {
+        if (err) { res.send(500, err);}
+
+        console.log('Created Person');
+
+        query = 'insert into shoe (personid,img,gender,size,type,org,local) values((select id from person where email = "' + email + '"),"' + img+ '","' + gender + '",' + size + ',"' + type + '","' + org +'","' + local +'");';
+        connection.query(query, function(err, rows, fields) {
+          if (err) { res.send(500, err);}
+
+          console.log('Introduced a shoe in new person');
+
+          res.send(200);
+        });
+      });
+
+    } else {
 
       query = 'insert into shoe (personid,img,gender,size,type,org,local) values((select id from person where email = "' + email + '"),"' + img+ '","' + gender + '",' + size + ',"' + type + '","' + org +'","' + local +'");';
 
       connection.query(query, function(err, rows, fields) {
-        if (err) throw err;
+        if (err) { res.send(500, err);}
+
+        console.log('Introduced a shoe in existing person');
         res.send(200);
       });
-    });
+    }
+
+  });
 
 });
 
@@ -68,7 +97,8 @@ app.post('/ask', function (req, res) {
   var query = 'SELECT * FROM shoe JOIN person ON person.id=shoe.personid WHERE person.email="' + email +'";';
 
   connection.query(query, function(err, rows, fields) {
-    if (err) throw err;
+    if (err) { res.send(500, err);}
+    console.log('Ask');
     res.send(rows);
   });
 });
